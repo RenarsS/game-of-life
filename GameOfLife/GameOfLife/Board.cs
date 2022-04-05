2c08a8d68e06d3fc55b301c7d67a42573baa7db1
Renars <renars.susejs@gmail.com>
2022-04-05 17:58:45 +0300

    Created prototype of board class of cells

---
+++ GameOfLife/GameOfLife/Board.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GameOfLife
+{
+    internal class Board
+    {
+        /// <summary>
+        /// Two-dimensional array in which initial values are stored
+        /// and new layout created.
+        /// </summary>
+        private bool[,] initalBoard;
+
+        public Board(int height, int width)
+        {
+            initalBoard = new bool[height, width];
+            populateBoard();
+        }
+
+        /// <summary>
+        /// Dummy value generation. Not random
+        /// </summary>
+        private void populateBoard()
+        {
+            for(int i = 0; i < initalBoard.GetLength(0); i++)
+            {
+                for(var j = 0; j < initalBoard.GetLength(1); j++)
+                {
+                    initalBoard[i, j] = (i+j) % 2 == 0 ? true : false;
+                }
+            }
+        }
+
+        /// <summary>
+        /// Iterates whole board of cells one time. 
+        /// Results in newer generation based on rules
+        /// </summary>
+        /// <returns></returns>
+        public bool[,] Iterate()
+        {
+            bool[,] newBoard = new bool[initalBoard.GetLength(0), initalBoard.GetLength(1)];
+
+            for(int i = 0; i < initalBoard.GetLength(0); i++)
+            {
+                for (int j = 0; j < initalBoard.GetLength(1); j++)
+                {
+                    bool initialCell = initalBoard[i, j];
+
+
+                }
+            }
+            return newBoard;
+        }
+
+
+
+        /// <summary>
+        /// Displays board on console
+        /// </summary>
+        public void DisplayBoard()
+        {
+            for (int i = 0; i < initalBoard.GetLength(0); i++)
+            {
+                for (var j = 0; j < initalBoard.GetLength(1); j++)
+                {
+                    Console.Write(initalBoard[i, j] ? 1 : 0);
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
--- GameOfLife/GameOfLife/Program.cs
+++ GameOfLife/GameOfLife/Program.cs
@@ -1,2 +1,16 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using GameOfLife;
+
+Console.WriteLine("Hello! This is Conway's Game of Life.");
+Console.WriteLine();
+
+Console.WriteLine("Enter height: ");
+string height = Console.ReadLine();
+
+Console.WriteLine("Enter width please");
+string width = Console.ReadLine();
+
+Console.Clear();
+
+var board = new Board(Int32.Parse(height), Int32.Parse(width));
+board.DisplayBoard();
