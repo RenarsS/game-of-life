590edccecb4a793faca4e3112655ae6987413da2
Renars <renars.susejs@gmail.com>
2022-05-06 17:31:44 +0300

    Removed redundant interface

--- GameOfLife/GameOfLife/Board/IBoard.cs
+++ GameOfLife/GameOfLife/Board/IBoard.cs
@@ -4,8 +4,23 @@ namespace GameOfLife
     /// <summary>
     /// Interface for board game functionality implementations.
     /// </summary>
-    public interface IBoard : IBoardBase
+    public interface IBoard
     {
+        /// <summary>
+        /// Unique identifier of the id.
+        /// </summary>
+        public Guid BoardId { get; }
+
+        /// <summary>
+        /// Holds statistics about board.
+        /// </summary>
+        public Dictionary<string, int> Statistics { get; set; }
+
+        /// <summary>
+        /// Layout of the board with recent generation.
+        /// </summary>
+        public bool[,] InitialBoard { get; set; }
+
         /// <summary>
         /// Algorithm and flow of the board.
         /// </summary>
